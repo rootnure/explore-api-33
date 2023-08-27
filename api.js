13a24df6ea08ec583833d4688f0dc6c70c2ef5bf
rootnure <nur.diu.2791@gmail.com>
2023-08-27 06:43:37 +0600

    Common concepts need to know for API

---
+++ api.js
@@ -0,0 +1,18 @@
+const user = {id: 1, name: 'Gorib Amir', job: 'Bakar'}
+console.log(user);
+
+// from now we will mostly use API for object value (JSON)
+
+/**
+ * // Common Questions
+ * 1. What is Internet?
+ * 2. What is IP (Internet Protocol) Address?
+ * 3. IPv4 vs IPv6
+ * 4. What is DNS (Domain Name System)? DNS Server.
+ * 5. How DNS Works?
+ * 6. How the Internet Works (User site, Server, Database)?
+ * 7. What is HTTP (Hyper Text Transfer Protocol)?
+ * 8. HTTP Layer (7 layers)
+ * 9. HTTP vs HTTPs
+ * 10. What is an API (Application Programming Interface)?
+ */
