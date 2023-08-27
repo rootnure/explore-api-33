aafa9c2a8c58df022cf88fdff70543cbd9330252
rootnure <nur.diu.2791@gmail.com>
2023-08-27 11:33:12 +0600

    Explore Create, Delete, Update post and API status code

--- js/index.js
+++ js/index.js
@@ -13,3 +13,39 @@ function displayUsers2(users){
         ul.appendChild(li);
     }
 }
+
+const deletePost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE',
+    });
+}
+
+const patchAPost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PATCH',
+        body: JSON.stringify({
+            title: 'foo',
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+const createAPost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
